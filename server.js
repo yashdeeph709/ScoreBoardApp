ce0ffc225e13636dacaddcff361554e55f097f9c
yashdeep hinge <yashdeeph709@gmail.com>
2015-03-13 02:31:33 +0530

    server update

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+//var express = require('express');
+//var app = express();
+//app.use(express.static('./public'));
+//app.get("*", function(req, res) {
+//    res.sendfile('public/index.html');
+//});
+
+//app.listen(3000);
 var express = require('express');
 var app = express();
-app.use(express.static('./public'));
-app.get("*", function(req, res) {
-    res.sendfile('public/index.html');
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
-app.listen(3000);
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
