a5f499f5acbfb2effda2e45f02cb707102388fc4
yashdeep hinge <yashdeeph709@gmail.com>
2015-03-12 15:31:51 +0530

    start championship tree created

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var api=require('./api');
 app.use(express.static('./public'));
+app.use('/api', api);
 app.get("*", function(req, res) {
     res.sendfile('public/index.html');
 });
