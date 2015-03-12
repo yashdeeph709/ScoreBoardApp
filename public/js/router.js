ac91ae852d46dd5e225380cc13f7f7705e741d96
yashdeep hinge <yashdeeph709@gmail.com>
2015-03-13 02:21:44 +0530

    changes done!

--- public/js/router.js
+++ public/js/router.js
@@ -2,11 +2,11 @@ var app=angular.module('ScoreBoardApp');
 app.config(router);
 
 function router($routeProvider,$locationProvider){
-$locationProvider.html5Mode(true);
 $routeProvider.when('/',{templateUrl:'partials/Home.html'});
 $routeProvider.when('/start',{templateUrl:'partials/MatchStart.html'});
 $routeProvider.when('/watch',{templateUrl:'partials/watch.html'});
 $routeProvider.when('/loop',{templateUrl:'partials/MatchLoop.html'});
 $routeProvider.when('/championship',{templateUrl:'partials/championship.html'});
+$routeProvider.when('/credits',{templateUrl:'partials/credits.html'});
 }
 
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
-var api=require('./api');
 app.use(express.static('./public'));
-app.use('/api', api);
 app.get("*", function(req, res) {
     res.sendfile('public/index.html');
 });
