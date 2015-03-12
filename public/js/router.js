79f5bc09702b956bbdeab5b568223137e510d2af
yashdeep hinge <yashdeeph709@gmail.com>
2015-03-13 02:44:45 +0530

    home route modified

--- public/js/router.js
+++ public/js/router.js
@@ -2,7 +2,7 @@ var app=angular.module('ScoreBoardApp');
 app.config(router);
 
 function router($routeProvider,$locationProvider){
-$routeProvider.when('/',{templateUrl:'partials/Home.html'});
+$routeProvider.when('/',{templateUrl:'partials/main.html'});
 $routeProvider.when('/start',{templateUrl:'partials/MatchStart.html'});
 $routeProvider.when('/watch',{templateUrl:'partials/watch.html'});
 $routeProvider.when('/loop',{templateUrl:'partials/MatchLoop.html'});
