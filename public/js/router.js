4697877439bd07d8cd1ca9bd0b740c7f8e48d7e2
unknown <yashdeeph709@gmail.com>
2015-03-15 22:43:12 +0530

    team entry done!

--- public/js/router.js
+++ public/js/router.js
@@ -2,11 +2,12 @@ var app=angular.module('ScoreBoardApp');
 app.config(router);
 
 function router($routeProvider,$locationProvider){
-$routeProvider.when('/',{templateUrl:'partials/main.html'});
+$routeProvider.when('/',{templateUrl:'partials/Main.html'});
 $routeProvider.when('/start',{templateUrl:'partials/MatchStart.html'});
-$routeProvider.when('/watch',{templateUrl:'partials/watch.html'});
+$routeProvider.when('/watch',{templateUrl:'partials/Watch.html'});
 $routeProvider.when('/loop',{templateUrl:'partials/MatchLoop.html'});
-$routeProvider.when('/championship',{templateUrl:'partials/championship.html'});
-$routeProvider.when('/credits',{templateUrl:'partials/credits.html'});
+$routeProvider.when('/championship',{templateUrl:'partials/Championship.html'});
+$routeProvider.when('/teamentry',{templateUrl:'partials/TeamEntry.html'});
+$routeProvider.when('/credits',{templateUrl:'partials/Credits.html'});
 }
 
--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-//var express = require('express');
-//var app = express();
-//app.use(express.static('./public'));
-//app.get("*", function(req, res) {
-//    res.sendfile('public/index.html');
-//});
-
-//app.listen(3000);
 var express = require('express');
 var app = express();
 
