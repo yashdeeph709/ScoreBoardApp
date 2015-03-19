33abb8cb1d47f48762f63b003bf68455719cea36
unknown <yashdeeph709@gmail.com>
2015-03-19 17:34:04 +0530

    static serve added

--- public/js/router.js
+++ public/js/router.js
@@ -2,11 +2,13 @@ var app=angular.module('ScoreBoardApp');
 app.config(router);
 
 function router($routeProvider,$locationProvider){
+$locationProvider.html5Mode(true);
 $routeProvider.when('/',{templateUrl:'partials/Main.html'});
 $routeProvider.when('/start',{templateUrl:'partials/MatchStart.html'});
 $routeProvider.when('/watch',{templateUrl:'partials/Watch.html'});
 $routeProvider.when('/loop',{templateUrl:'partials/MatchLoop.html'});
 $routeProvider.when('/championship',{templateUrl:'partials/Championship.html'});
+$routeProvider.when('/teamlist',{templateUrl:'partials/TeamList.html'});
 $routeProvider.when('/teamentry',{templateUrl:'partials/TeamEntry.html'});
 $routeProvider.when('/credits',{templateUrl:'partials/Credits.html'});
 }
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var express = require('express');
 var app = express();
-
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public/index.html'));
-
-
+app.use(express.static(__dirname + '/public'));
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
