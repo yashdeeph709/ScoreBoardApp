2628062548409d2c453791905d58cc1fc60b5df0
unknown <yashdeeph709@gmail.com>
2015-03-19 23:44:56 +0530

    issue resolved

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
-app.use(express.static('./'));
-app.use('/partials',express.static(path.join(__dirname, '/partials')));
+
 app.set('port', (process.env.PORT || 5000));
+app.use('/',express.static('./public'));
+app.use('/partials',express.static(__dirname+'./partials'));
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
