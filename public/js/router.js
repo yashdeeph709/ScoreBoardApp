495fa1a7c2186d91b8bba1b6b5391d094cc9d7ce
unknown <yashdeeph709@gmail.com>
2015-03-31 15:55:08 +0530

    ui router applied

--- public/js/router.js
+++ public/js/router.js
@@ -1,17 +1,18 @@
 var app=angular.module('ScoreBoardApp');
 app.config(router);
 
-function router($routeProvider){
-$routeProvider.when('/',{templateUrl:'partials/home.html'});
-$routeProvider.when('/dashboard',{templateUrl:'partials/Dashboard.html'});
-$routeProvider.when('/start',{templateUrl:'partials/MatchStart.html'});
-$routeProvider.when('/joinus',{templateUrl:'partials/JoinUs.html'});
-$routeProvider.when('/watch',{templateUrl:'partials/Watch.html'});
-$routeProvider.when('/loop',{templateUrl:'partials/MatchLoop.html'});
-$routeProvider.when('/championship',{templateUrl:'partials/createCship.html'});
-$routeProvider.when('/teamlist',{templateUrl:'partials/TeamList.html'});
-$routeProvider.when('/teamentry',{templateUrl:'partials/TeamEntry.html'});
-$routeProvider.when('/playerentry/:teamid',{templateUrl:'partials/PlayerEntry.html'});
-$routeProvider.when('/credits',{templateUrl:'partials/credit.html'});
+function router($stateProvider){
+$stateProvider.state('home',{'url':'/',templateUrl:'partials/home.html'});
+$stateProvider.state('dashboard',{'url':'/dashboard',templateUrl:'partials/Dashboard.html'});
+$stateProvider.state('matchstart',{'url':'/start',templateUrl:'partials/MatchStart.html'});
+$stateProvider.state('joinus',{'url':'/joinus',templateUrl:'partials/JoinUs.html'});
+$stateProvider.state('watch',{'url':'/watch',templateUrl:'partials/Watch.html'});
+$stateProvider.state('loop',{'url':'/loop',templateUrl:'partials/MatchLoop.html'});
+$stateProvider.state('createcship',{'url':'/championship',templateUrl:'partials/createCship.html'});
+$stateProvider.state('teamlist',{'url':'/teamlist',templateUrl:'partials/TeamList.html'});
+$stateProvider.state('teamentry',{'url':'/teamentry',templateUrl:'partials/TeamEntry.html'});
+$stateProvider.state('playerentry',{'url':'/playerentry/:teamid',templateUrl:'partials/PlayerEntry.html'});
+$stateProvider.state('credits',{'url':'/credits',templateUrl:'partials/credit.html'});
+$stateProvider.otherwise({templateUrl:'partials/404.html'});
 }
 
