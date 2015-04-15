260d3235e600847da30a902b58763037b778ce6f
unknown <yashdeeph709@gmail.com>
2015-04-15 23:23:55 +0530

    config retry

--- api.js
+++ api.js
@@ -3,15 +3,8 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var championship = require('./models/championship');
 var os = require('os');
-/*if(process.env.PORT!=5000){
-    console.log('wrong try');
-//    mongoose.connect("mongodb://localhost:27017/scoreboard");
-}else{
-    console.log('right try');
-//mongoose.connect("mongodb://UiOcsrnnsovG:wDjzlUlcKbgd@mongosoup-cont002.mongosoup.de:32546/cc_UiOcsrnnsovG");
-*/
 mongoose.connect(process.env.MONGOLAB_URI);
-//}
+
 router.post('/championship', function(req, res) {
     var newChampionship = new championship({
         "user": "default",
