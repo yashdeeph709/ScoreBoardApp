9b93b0dceb6d4d7143fe9fb8e194edca69c424fe
unknown <yashdeeph709@gmail.com>
2015-04-26 11:33:06 +0530

    login ctrl issue

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ app.set('port', (process.env.PORT || 5000));
 app.use(bodyParser.json());
 app.use(express.static('./public'));
 app.use('/partials', express.static(__dirname + './partials'));
+app.use('/js/services', express.static(__dirname + './js/services'));
+
 app.use(function(req,res,next){
+
 	res.header('Access-Control-Allow-Origin','*');
 	res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
 	res.header('Access-Control-Allow-Headers','Content-Type,Authorization');
